3cf2795ef3b441fa05ab0c6e95d99d184533469e
haxeleet <haxeleet@icloud.com>
2024-06-18 06:35:33 +0900

    daemon create & net running

--- src/main.c
+++ src/main.c
@@ -2,15 +2,10 @@
 
 #include "daemon.h"
 
-static daemon_t daemon;
-
-void init() {
-	daemon.net = net_create();
-}
-
 int main() {
-	printf("Hello world!!!\n");
+	daemon_create();
 
+	net_running(daemon->net);
 	while(1);
 
 	return 0;
