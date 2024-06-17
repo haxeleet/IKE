ffe14fec4321ba1b52b39033ed8e5910c895f6a9
haxeleet <haxeleet@icloud.com>
2024-06-18 02:11:06 +0900

    daemon

---
+++ include/daemon.h
@@ -0,0 +1,10 @@
+#ifndef __DAEMON_H__
+#define __DAEMON_H__
+
+#include "network.h"
+
+typedef struct {
+  network_t* net;
+}daemon_t;
+
+#endif //__DAEMON_H__
--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+#include "daemon.h"
+
+static daemon_t daemon;
+
+void init() {
+	daemon.net = net_create();
+}
+
 int main() {
 	printf("Hello world!!!\n");
+
+	while(1);
 
 	return 0;
 }
